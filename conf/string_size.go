6174858cd3d843843dedb1a6d49c89d8bb81fd7a
lh <35221199@qq.com>
2023-09-20 16:52:23 +0800

    fix

--- conf/string_size.go
+++ conf/string_size.go
@@ -1,36 +1,30 @@
 package conf
 
-import (
-	"encoding/json"
-
-	"code.cloudfoundry.org/bytefmt"
-)
-
 // StringSize is a size that is unmarshaled from a string.
-type StringSize uint64
+// type StringSize uint64
 
-// MarshalJSON implements json.Marshaler.
-func (s StringSize) MarshalJSON() ([]byte, error) {
-	return []byte(`"` + bytefmt.ByteSize(uint64(s)) + `"`), nil
-}
+// // MarshalJSON implements json.Marshaler.
+// func (s StringSize) MarshalJSON() ([]byte, error) {
+// 	return []byte(`"` + bytefmt.ByteSize(uint64(s)) + `"`), nil
+// }
 
-// UnmarshalJSON implements json.Unmarshaler.
-func (s *StringSize) UnmarshalJSON(b []byte) error {
-	var in string
-	if err := json.Unmarshal(b, &in); err != nil {
-		return err
-	}
+// // UnmarshalJSON implements json.Unmarshaler.
+// func (s *StringSize) UnmarshalJSON(b []byte) error {
+// 	var in string
+// 	if err := json.Unmarshal(b, &in); err != nil {
+// 		return err
+// 	}
 
-	v, err := bytefmt.ToBytes(in)
-	if err != nil {
-		return err
-	}
+// 	v, err := bytefmt.ToBytes(in)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	*s = StringSize(v)
-	return nil
-}
+// 	*s = StringSize(v)
+// 	return nil
+// }
 
-// UnmarshalEnv implements envUnmarshaler.
-func (s *StringSize) UnmarshalEnv(v string) error {
-	return s.UnmarshalJSON([]byte(`"` + v + `"`))
-}
+// // UnmarshalEnv implements envUnmarshaler.
+// func (s *StringSize) UnmarshalEnv(v string) error {
+// 	return s.UnmarshalJSON([]byte(`"` + v + `"`))
+// }
--- go.mod
+++ go.mod
@@ -8,10 +8,8 @@ require (
 	github.com/bluenviron/gortsplib/v4 v4.0.1
 	github.com/bluenviron/mediacommon v1.1.1
 	github.com/datarhei/gosrt v0.5.4
-	github.com/fsnotify/fsnotify v1.6.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/google/uuid v1.3.1
-	github.com/gookit/color v1.5.4
 	github.com/gorilla/websocket v1.5.0
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 	github.com/notedit/rtmp v0.0.2
@@ -24,7 +22,6 @@ require (
 	github.com/stretchr/testify v1.8.4
 	golang.org/x/crypto v0.13.0
 	golang.org/x/net v0.15.0
-	golang.org/x/term v0.12.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
@@ -35,6 +32,7 @@ require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -64,7 +62,6 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
-	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
